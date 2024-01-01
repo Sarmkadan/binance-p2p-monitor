96b94ca31c7cfa8c713d1c92b6a63d2a75ae7545
Vladyslav Zaiets <rutova2@gmail.com>
2024-01-01 19:42:39 +0200

    wip

--- src/Workers/StatisticsCollectorWorker.cs
+++ src/Workers/StatisticsCollectorWorker.cs
@@ -58,8 +58,8 @@ public class StatisticsCollectorWorker : BackgroundService
         var priceRepository = scope.ServiceProvider.GetRequiredService<IPriceRepository>();
         var historyRepository = scope.ServiceProvider.GetRequiredService<IHistoryRepository>();
 
-        var assets = _appSettings.MonitoredAssets.FirstOrDefault()?.Split(',') ?? string[]();
-        var fiats = _appSettings.MonitoredFiats.FirstOrDefault()?.Split(',') ?? string[]();
+        var assets = _appSettings.MonitoredAssets.FirstOrDefault()?.Split(',') ?? Array.Empty<string>();
+        var fiats = _appSettings.MonitoredFiats.FirstOrDefault()?.Split(',') ?? Array.Empty<string>();
 
         _logger.LogDebug("Collecting statistics for {AssetCount} assets and {FiatCount} fiats", assets.Length, fiats.Length);
 
