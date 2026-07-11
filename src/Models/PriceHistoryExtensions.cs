651d794aa006c2912442af9ce8fe4621375ec98b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 04:59:52 +0000

    feat(models): add PriceHistory extension helpers

---
+++ src/Models/PriceHistoryExtensions.cs
@@ -0,0 +1,49 @@
+namespace BinanceP2pMonitor.Models;
+
+/// <summary>
+/// Provides extension methods for <see cref="PriceHistory"/>.
+/// </summary>
+public static class PriceHistoryExtensions
+{
+    /// <summary>
+    /// Determines if the price history record is older than a specified number of minutes.
+    /// </summary>
+    /// <param name="priceHistory">The price history record.</param>
+    /// <param name="minutes">The number of minutes.</param>
+    /// <returns><c>true</c> if the price history record is older than the specified number of minutes; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="priceHistory"/> is <c>null</c>.</exception>
+    public static bool IsOlderThan(this PriceHistory priceHistory, int minutes)
+    {
+        ArgumentNullException.ThrowIfNull(priceHistory);
+
+        return priceHistory.GetAgeInMinutes() > minutes;
+    }
+
+    /// <summary>
+    /// Gets the price history records that are within a specified time range.
+    /// </summary>
+    /// <param name="priceHistories">The collection of price history records.</param>
+    /// <param name="startDate">The start date of the time range.</param>
+    /// <param name="endDate">The end date of the time range.</param>
+    /// <returns>An <see cref="IReadOnlyList{PriceHistory}"/> of price history records within the specified time range.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="priceHistories"/> is <c>null</c>.</exception>
+    public static IReadOnlyList<PriceHistory> GetInTimeRange(this IEnumerable<PriceHistory> priceHistories, DateTime startDate, DateTime endDate)
+    {
+        ArgumentNullException.ThrowIfNull(priceHistories);
+
+        return priceHistories.Where(ph => ph.RecordedAt >= startDate && ph.RecordedAt <= endDate).ToList();
+    }
+
+    /// <summary>
+    /// Calculates the average price change percentage for a collection of price history records.
+    /// </summary>
+    /// <param name="priceHistories">The collection of price history records.</param>
+    /// <returns>The average price change percentage.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="priceHistories"/> is <c>null</c>.</exception>
+    public static decimal CalculateAveragePriceChangePercentage(this IEnumerable<PriceHistory> priceHistories)
+    {
+        ArgumentNullException.ThrowIfNull(priceHistories);
+
+        return priceHistories.Average(ph => ph.PriceChangePercent);
+    }
+}
