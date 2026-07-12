c06a878e248c79bb75a905ff5144f15ce3b1b551
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-12 02:19:25 +0000

    docs(exceptions): document SerializationException public API

--- src/Exceptions/SerializationExceptionExtensions.cs
+++ src/Exceptions/SerializationExceptionExtensions.cs
@@ -4,15 +4,21 @@ namespace BinanceP2pMonitor.Exceptions;
 
 /// <summary>
 /// Provides extension methods for <see cref="SerializationException"/> and its derived types
+/// to extract detailed information about serialization errors.
 /// </summary>
 public static class SerializationExceptionExtensions
 {
 	/// <summary>
-	/// Determines whether the exception is a <see cref="JsonSerializationException"/>
+	/// Determines whether the specified exception is of type <see cref="JsonSerializationException"/>.
 	/// </summary>
-	/// <param name="exception">The exception to check</param>
-	/// <returns>True if the exception is a JsonSerializationException; otherwise, false</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null</exception>
+	/// <param name="exception">The <see cref="SerializationException"/> to check.</param>
+	/// <returns>
+	/// <c>true</c> if <paramref name="exception"/> is a <see cref="JsonSerializationException"/>; 
+	/// otherwise, <c>false</c>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	/// Thrown when <paramref name="exception"/> is <c>null</c>.
+	/// </exception>
 	public static bool IsJsonSerializationException(this SerializationException exception)
 	{
 		ArgumentNullException.ThrowIfNull(exception);
@@ -20,11 +26,17 @@ public static class SerializationExceptionExtensions
 	}
 
 	/// <summary>
-	/// Gets a detailed error message including the exception message, data type, and JSON content (if available)
+	/// Gets a detailed error message that includes the exception message, data type, and JSON content
+	/// (if available) for the specified <see cref="SerializationException"/>.
 	/// </summary>
-	/// <param name="exception">The serialization exception</param>
-	/// <returns>A formatted error message with detailed information</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null</exception>
+	/// <param name="exception">The <see cref="SerializationException"/> to process.</param>
+	/// <returns>
+	/// A formatted string containing detailed error information. For <see cref="JsonSerializationException"/>,
+	/// includes <see cref="JsonSerializationException.DataType"/> and <see cref="JsonSerializationException.JsonContent"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	/// Thrown when <paramref name="exception"/> is <c>null</c>.
+	/// </exception>
 	public static string GetDetailedMessage(this SerializationException exception)
 	{
 		ArgumentNullException.ThrowIfNull(exception);
@@ -37,11 +49,16 @@ public static class SerializationExceptionExtensions
 	}
 
 	/// <summary>
-	/// Determines whether the exception contains JSON content
+	/// Determines whether the specified <see cref="SerializationException"/> contains non-null JSON content.
 	/// </summary>
-	/// <param name="exception">The serialization exception to check</param>
-	/// <returns>True if the exception is a JsonSerializationException with non-null JsonContent; otherwise, false</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null</exception>
+	/// <param name="exception">The <see cref="SerializationException"/> to check.</param>
+	/// <returns>
+	/// <c>true</c> if <paramref name="exception"/> is a <see cref="JsonSerializationException"/> 
+	/// and <see cref="JsonSerializationException.JsonContent"/> is not <c>null</c>; otherwise, <c>false</c>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	/// Thrown when <paramref name="exception"/> is <c>null</c>.
+	/// </exception>
 	public static bool HasJsonContent(this SerializationException exception)
 	{
 		ArgumentNullException.ThrowIfNull(exception);
