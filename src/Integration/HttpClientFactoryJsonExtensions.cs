ff296bf0730c78f98525eb586714857252dfe7bb
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 16:31:40 +0300

    refactor: polish HttpClientFactoryJsonExtensions

--- src/Integration/HttpClientFactoryJsonExtensions.cs
+++ src/Integration/HttpClientFactoryJsonExtensions.cs
@@ -1,9 +1,12 @@
 #nullable enable
 
+using System;
+using System.Text.Json;
+
 namespace BinanceP2pMonitor.Integration;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for <see cref="HttpClientFactory"/>
+/// Provides System.Text.Json serialization extensions for working with JSON data models.
 /// </summary>
 public static class HttpClientFactoryJsonExtensions
 {
@@ -14,13 +17,13 @@ public static class HttpClientFactoryJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the <see cref="HttpClientFactory"/> instance to a JSON string.
+    /// Serializes an object to a JSON string.
     /// </summary>
-    /// <param name="value">The instance to serialize</param>
+    /// <param name="value">The object to serialize</param>
     /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>A JSON string representation of the instance</returns>
+    /// <returns>A JSON string representation of the object</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
-    public static string ToJson(this HttpClientFactory value, bool indented = false)
+    public static string ToJson(this object value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -28,38 +31,40 @@ public static class HttpClientFactoryJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="HttpClientFactory"/> instance.
+    /// Deserializes a JSON string to an instance of type <typeparamref name="T"/>.
     /// </summary>
+    /// <typeparam name="T">The type to deserialize the JSON into</typeparam>
     /// <param name="json">The JSON string to deserialize</param>
     /// <returns>The deserialized instance, or null if the JSON represents a null value</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized</exception>
-    public static HttpClientFactory? FromJson(string json)
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized into type <typeparamref name="T"/></exception>
+    public static T? FromJson<T>(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
 
-        return JsonSerializer.Deserialize<HttpClientFactory>(json, _jsonOptions);
+        return JsonSerializer.Deserialize<T>(json, _jsonOptions);
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="HttpClientFactory"/> instance.
+    /// Attempts to deserialize a JSON string to an instance of type <typeparamref name="T"/>.
     /// </summary>
+    /// <typeparam name="T">The type to deserialize the JSON into</typeparam>
     /// <param name="json">The JSON string to deserialize</param>
     /// <param name="value">The deserialized instance, or null if deserialization failed</param>
     /// <returns>True if deserialization succeeded; otherwise, false</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
-    public static bool TryFromJson(string json, out HttpClientFactory? value)
+    public static bool TryFromJson<T>(string json, out T? value)
     {
         ArgumentNullException.ThrowIfNull(json);
 
         try
         {
-            value = JsonSerializer.Deserialize<HttpClientFactory>(json, _jsonOptions);
+            value = JsonSerializer.Deserialize<T>(json, _jsonOptions);
             return true;
         }
         catch (JsonException)
         {
-            value = null;
+            value = default;
             return false;
         }
     }
