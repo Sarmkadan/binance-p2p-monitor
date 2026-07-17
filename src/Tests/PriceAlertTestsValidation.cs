cb5a647e6c500ac7cd4e9fc94eb60565fe920782
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    docs: add XML documentation to PriceAlertTestsValidation class and methods

--- src/Tests/PriceAlertTestsValidation.cs
+++ src/Tests/PriceAlertTestsValidation.cs
@@ -2,22 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// Provides validation methods for <see cref="PriceAlert"/> objects to ensure they meet expected test criteria.
+/// </summary>
 public static class PriceAlertTestsValidation
 {
-    public static IReadOnlyList<string> Validate(PriceAlert value)
+    	/// <summary>
+	/// Validates a <see cref="PriceAlert"/> object and returns a list of validation error messages.
+	/// </summary>
+	/// <param name="value">The PriceAlert object to validate.</param>
+	/// <returns>
+	/// An <see cref="IReadOnlyList{T}"/> of error messages. If the list is empty, the PriceAlert is valid.
+	/// </returns>
+	public static IReadOnlyList<string> Validate(PriceAlert value)
     {
         var errors = new List<string>();
         // Add validation logic here
         return errors;
     }
 
-    public static bool IsValid(PriceAlert value)
+    	/// <summary>
+	/// Determines whether a <see cref="PriceAlert"/> object is valid according to test criteria.
+	/// </summary>
+	/// <param name="value">The PriceAlert object to check.</param>
+	/// <returns>
+	/// <see langword="true"/> if the PriceAlert is valid; otherwise, <see langword="false"/>.
+	/// </returns>
+	public static bool IsValid(PriceAlert value)
     {
         // Add validation logic here
         return true;
     }
 
-    public static void EnsureValid(PriceAlert value)
+    	/// <summary>
+	/// Ensures that a <see cref="PriceAlert"/> object is valid according to test criteria.
+	/// </summary>
+	/// <param name="value">The PriceAlert object to validate.</param>
+	/// <exception cref="Exception">
+	/// Thrown when the PriceAlert object is invalid with the message "PriceAlert object is invalid".
+	/// </exception>
+	public static void EnsureValid(PriceAlert value)
     {
         if (!IsValid(value))
         {
