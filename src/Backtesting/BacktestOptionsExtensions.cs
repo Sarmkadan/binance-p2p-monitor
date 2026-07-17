5ab0a9543cebd1d6005132cc6d300ca3f997baed
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 18:04:21 +0300

    small refactor - BacktestOptionsExtensions

--- src/Backtesting/BacktestOptionsExtensions.cs
+++ src/Backtesting/BacktestOptionsExtensions.cs
@@ -19,18 +19,43 @@ public static class BacktestOptionsExtensions
     }
 
     /// <summary>
-    /// Determines if the stop loss or take profit should be triggered based on the current spread and thresholds.
+    /// Determines if the stop loss should be triggered based on the current spread.
     /// </summary>
     /// <param name="options">The backtest options.</param>
     /// <param name="currentSpread">The current spread.</param>
-    /// <returns><c>true</c> if the stop loss or take profit should be triggered; otherwise, <c>false</c>.</returns>
+    /// <returns><c>true</c> if the stop loss should be triggered; otherwise, <c>false</c>.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="options"/> is null.</exception>
-    public static bool ShouldTriggerStopLossOrTakeProfit(this BacktestOptions options, decimal currentSpread)
+    public static bool ShouldTriggerStopLoss(this BacktestOptions options, decimal currentSpread)
     {
         ArgumentNullException.ThrowIfNull(options);
 
-        return currentSpread <= options.EntrySpreadThreshold - options.StopLossPercent
-            || currentSpread >= options.ExitSpreadThreshold + options.TakeProfitPercent;
+        return currentSpread <= options.EntrySpreadThreshold - options.StopLossPercent;
+    }
+
+    /// <summary>
+    /// Determines if the take profit should be triggered based on the current spread.
+    /// </summary>
+    /// <param name="options">The backtest options.</param>
+    /// <param name="currentSpread">The current spread.</param>
+    /// <returns><c>true</c> if the take profit should be triggered; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="options"/> is null.</exception>
+    public static bool ShouldTriggerTakeProfit(this BacktestOptions options, decimal currentSpread)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        return currentSpread >= options.ExitSpreadThreshold + options.TakeProfitPercent;
+    }
+
+    /// <summary>
+    /// Determines if either the stop loss or take profit should be triggered based on the current spread and thresholds.
+    /// </summary>
+    /// <param name="options">The backtest options.</param>
+    /// <param name="currentSpread">The current spread.</param>
+    /// <returns><c>true</c> if either the stop loss or take profit should be triggered; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="options"/> is null.</exception>
+    public static bool ShouldTriggerStopLossOrTakeProfit(this BacktestOptions options, decimal currentSpread)
+    {
+        return options.ShouldTriggerStopLoss(currentSpread) || options.ShouldTriggerTakeProfit(currentSpread);
     }
 
     /// <summary>
@@ -40,9 +65,11 @@ public static class BacktestOptionsExtensions
     /// <param name="positionSize">The position size.</param>
     /// <returns>The transaction cost.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="options"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="positionSize"/> is negative.</exception>
     public static decimal CalculateTransactionCost(this BacktestOptions options, decimal positionSize)
     {
         ArgumentNullException.ThrowIfNull(options);
+        ArgumentOutOfRangeException.ThrowIfNegative(positionSize);
 
         return positionSize * options.TransactionCostPercent / 100;
     }
