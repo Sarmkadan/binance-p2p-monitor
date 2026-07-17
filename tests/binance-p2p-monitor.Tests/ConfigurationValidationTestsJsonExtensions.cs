b40e6fffb6213d5dd833583f595e234d0cf21098
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor(binance-p2p-monitor.tests): small refactor - ConfigurationValid

--- tests/binance-p2p-monitor.Tests/ConfigurationValidationTestsJsonExtensions.cs
+++ tests/binance-p2p-monitor.Tests/ConfigurationValidationTestsJsonExtensions.cs
@@ -11,6 +11,7 @@ namespace BinanceP2pMonitor.Tests;
 /// </summary>
 public static class ConfigurationValidationTestsJsonExtensions
 {
+
     /// <summary>
     /// JSON serialization options configured for camelCase property naming.
     /// </summary>
@@ -28,16 +29,13 @@ public static class ConfigurationValidationTestsJsonExtensions
     /// <param name="value">The instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this ConfigurationValidationTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
         var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
             : _jsonSerializerOptions;
 
         return JsonSerializer.Serialize(value, options);
@@ -47,8 +45,9 @@ public static class ConfigurationValidationTestsJsonExtensions
     /// Deserializes a JSON string to a <see cref="ConfigurationValidationTests"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized instance, or null if the JSON is empty or whitespace.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    /// <returns>The deserialized instance.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</exception>
+    /// <exception cref="JsonException">The JSON is invalid or cannot be deserialized.</exception>
     public static ConfigurationValidationTests? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
@@ -60,8 +59,9 @@ public static class ConfigurationValidationTestsJsonExtensions
     /// Attempts to deserialize a JSON string to a <see cref="ConfigurationValidationTests"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized instance if successful, otherwise null.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <param name="value">Receives the deserialized instance if successful, otherwise <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</exception>
     public static bool TryFromJson(string json, out ConfigurationValidationTests? value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
